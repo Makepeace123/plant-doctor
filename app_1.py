844c743b660dcb28fc0ec46c3b2ce8929a81f015
Makepeace123 <makepeacebanelez@gmail.com>
2025-05-17 05:57:49 -0700

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -196,9 +196,6 @@ def display_results(predicted_class, info, confidence):
                 **💰Approx. Market Price:**  
                 *{chem.get('price', 'Not available')}* 
                 
-                **⚖️Application Frequency:**  
-                *{chem.get('frequency', 'As per the chemical instructions')}*  
-                
                 **⚠️Safety Precautions:**  
                 *{chem.get('safety', 'Wear protective gear during application')}*
                 """)
